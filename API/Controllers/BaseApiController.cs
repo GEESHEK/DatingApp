78c482118a4e5cb97ba72a8c8397ca723eb4d693
Gee Chong Shek <geeshek95@outlook.com>
2024-02-08 20:12:46 +0000

    created base api controller

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users (class name minus the word controller)
+public class BaseApiController : ControllerBase
+{
+    
+}
