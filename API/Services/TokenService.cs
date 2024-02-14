f1521c3637f11074c2e3cb6e5bbf94e87c483986
Gee Chong Shek <geeshek95@outlook.com>
2024-02-14 14:13:50 +0000

    added the token service, token logic, user DTO and refactored the controllers to return the user DTO instead of returning the userApp object

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService : ITokenService
+{
+    //this key stays on the server 
+    private readonly SymmetricSecurityKey _key; 
+    
+    public TokenService(IConfiguration config)
+    {
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+    }
+    
+    public string CreateToken(AppUser user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+        };
+
+        //what we sign the token with and what algorithm to encrypt the key with
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+        
+        //describe the token we are going to return
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
