e02fb4a4bfea3bfdf5020aa43d53412446e0f5f7
Gee Chong Shek <geeshek95@outlook.com>
2024-02-21 15:37:34 +0000

    home and register pages added

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  registerMode = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  model: any = {}
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+
+}
