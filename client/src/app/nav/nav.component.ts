2b622cd5112fee5b078a7f97a4ace5bde57fbea0
Gee Chong Shek <geeshek95@outlook.com>
2024-02-22 20:55:25 +0000

    routing and toast implemented

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Route, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,24 +13,22 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {}
   
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr:ToastrService) {}
   
   ngOnInit(): void {
   }
 
-
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
-      
+      next: () => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
