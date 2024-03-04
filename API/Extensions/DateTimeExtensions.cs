d162e2af43ee07fc7c6a7d1ed6201c0917df4f3c
Gee Chong Shek <geeshek95@outlook.com>
2024-03-04 11:55:31 +0000

    Extended the user entity with new properties, Added Age extension method, photo relationship and a new migration for these changes

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static int CalcuateAge(this DateOnly dob)
+    {   //not accounted for leap year etc, not 100%
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+        var age = today.Year - dob.Year;
+        //take a year off them if their birthday hasn't passed
+        if (dob > today.AddYears(-age)) age--;
+
+        return age;
+    } 
+}
