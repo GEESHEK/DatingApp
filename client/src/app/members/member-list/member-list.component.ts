d7a26ed177331cf1e0f5a5315d12228b87b4fdbe
Gee Chong Shek <geeshek95@outlook.com>
2024-03-08 15:05:14 +0000

    Retrieved the members list and displayed them in the members component. Also fixed home page authorisation error.

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  members: Member[] = [];
+
+  constructor(private memberService: MembersService) { }
+  //as soon as we call this route it will load the members
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: members => this.members = members
+    })
+  }
 
 }
