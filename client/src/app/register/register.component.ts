fbf21ffc30344659314fbec2603ca82e889b8670
Gee Chong Shek <geeshek95@outlook.com>
2024-03-21 17:11:53 +0000

    Using the form builder service Fixed password not working due to array

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -31,8 +31,8 @@ export class RegisterComponent implements OnInit {
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
-      password: ['', [Validators.required]
-        , Validators.minLength(4), Validators.maxLength(8)],
+      password: ['', [Validators.required
+        , Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]],
     });
     //if the password is changed after comfirm password we revalidate the comfirm password input
