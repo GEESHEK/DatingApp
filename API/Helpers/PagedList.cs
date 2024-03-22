a8f418d7cac2aae0c1129ceff14343dd97fa94c9
Gee Chong Shek <geeshek95@outlook.com>
2024-03-22 12:25:14 +0000

    Adding a paged list class

---
+++ API/Helpers/PagedList.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Helpers;
+
+public class PagedList<T> : List<T>
+{
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+
+    public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+    {
+        CurrentPage = pageNumber;
+        //10 / 4 = 2.1 > Ceiling makes it 3 pages 
+        TotalPages = (int) Math.Ceiling(count / (double) pageSize);
+        PageSize = pageSize;
+        TotalCount = count;
+        AddRange(items);
+    }
+
+    //pass this method our query > nothing has been executed yet 
+    //query being build up in memory by EF
+    //executed on count and ToList
+    public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+    {
+        //gives us a count of items for this specific query (total or filtered)
+        var count = await source.CountAsync();
+        //if you are on page 1 you don't want to skip
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        return new PagedList<T>(items, count, pageNumber, pageSize);
+    }
+}
