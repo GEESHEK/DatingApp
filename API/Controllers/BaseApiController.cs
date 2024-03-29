ba50a0304e60dae6272daa678a80005c9b0d9fd0
Gee Chong Shek <geeshek95@outlook.com>
2024-03-29 16:01:36 +0000

    Adding an action filter

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+[ServiceFilter(typeof(LogUserActivity))]
 [ApiController]
 [Route("api/[controller]")] // /api/users (class name minus the word controller)
 public class BaseApiController : ControllerBase
