59e8e6beff5a27a86319763252ab09ba9228e242
Gee Chong Shek <geeshek95@outlook.com>
2024-03-30 16:56:25 +0000

    Restoring caching for member detailed

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -18,7 +18,7 @@ export class MembersService {
   user: User | undefined;
   userParams: UserParams | undefined;
 
-  constructor(private http: HttpClient, private accountService: AccountService) { 
+  constructor(private http: HttpClient, private accountService: AccountService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user => {
         if (user) {
@@ -38,20 +38,20 @@ export class MembersService {
   }
 
   resetUserParams() {
-    if(this.user) {
+    if (this.user) {
       console.log("in reset filters");
       this.userParams = new UserParams(this.user);
       return this.userParams;
     }
     return; //if we don't have user object return nothing
   }
-  
+
 
   getMembers(UserParams: UserParams) {
     //to check if this query has been made before with the key (these params) 
     const response = this.memberCache.get(Object.values(UserParams).join('-'));
     //result will be in response if key match
-    if(response) return of(response);
+    if (response) return of(response);
 
     //allows us to send up the query params
     let params = this.getPaginationHeaders(UserParams.pageNumber, UserParams.pageSize);
@@ -62,11 +62,43 @@ export class MembersService {
     params = params.append('orderBy', UserParams.orderBy);
     //set result back in the member Cache
     return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(
-      map(response => {       
+      map(response => {
         this.memberCache.set(Object.values(UserParams).join('-'), response);
         return response; //return results so component can use it
       })
     );
+  }
+
+  getMember(username: string) {
+    //check the members list to see if we have the member
+    // const member = this.members.find(x => x.userName === username);
+    // if (member) return of(member);
+
+    const member = [...this.memberCache.values()]
+    .reduce((arr, elem) => arr.concat(elem.result), [])
+    .find((member: Member) => member.userName === username);
+    
+    if (member) return of(member);
+
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        //update the member in the array with the new updated values
+        this.members[index] = { ...this.members[index], ...member }
+      })
+    );
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {})
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId)
   }
 
   //making this method reusable with generics 
@@ -95,30 +127,5 @@ export class MembersService {
     params = params.append('pageSize', pageSize);
 
     return params;
-  }
-
-  getMember(username: string) {
-    //check the members list to see if we have the member
-    const member = this.members.find(x => x.userName === username);
-    if (member) return of(member);
-    return this.http.get<Member>(this.baseUrl + 'users/' + username);
-  }
-
-  updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member).pipe(
-      map(() => {
-        const index = this.members.indexOf(member);
-        //update the member in the array with the new updated values
-        this.members[index] = { ...this.members[index], ...member }
-      })
-    );
-  }
-
-  setMainPhoto(photoId: number) {
-    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {})
-  }
-
-  deletePhoto(photoId: number) {
-    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId)
   }
 }
