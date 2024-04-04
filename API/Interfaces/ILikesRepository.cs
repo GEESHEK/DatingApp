ec7a69e73ac15230547f95aec83932b4bb4d99f8
Gee Chong Shek <geeshek95@outlook.com>
2024-04-04 19:46:41 +0100

    Adding a likes repository

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces;
+
+public interface ILikesRepository
+{
+    Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+    Task<AppUser> GetUserWithLikes(int userId);
+    //to get the user they liked or liked by
+    Task<IEnumerable<LikeDto>> GetUserLikes(string predicate); 
+}
