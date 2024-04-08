276185a58b26d351f72728abc8d6f2abacd35ed6
Gee Chong Shek <geeshek95@outlook.com>
2024-04-08 08:11:46 +0100

    Setting up the entities for messaging

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,6 +11,7 @@ public class DataContext : DbContext
 
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     //override method to further configure the models
     protected override void OnModelCreating(ModelBuilder builder)
@@ -32,5 +33,16 @@ public class DataContext : DbContext
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
+        
+        //setting up messaging relationship 
+        builder.Entity<Message>()
+            .HasOne(u => u.Recipient)
+            .WithMany(m => m.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.Entity<Message>()
+            .HasOne(u => u.Sender)
+            .WithMany(m => m.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
