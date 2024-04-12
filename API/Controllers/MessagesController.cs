d08059807a4a6283674e00451f367fd2dc64ddf6
Gee Chong Shek <geeshek95@outlook.com>
2024-04-12 10:21:17 +0100

    Adding a message controller

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,58 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace API.Controllers;
+
+public class MessagesController : BaseApiController
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMessageRepository _messageRepository;
+    private readonly IMapper _mapper;
+
+    public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository, 
+        IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _messageRepository = messageRepository;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateMessage(CreateMessageDto createMessageDto)
+    {
+        //get username from the token
+        var username = User.GetUsername();
+
+        if (username == createMessageDto.RecipientUsername.ToLower())
+            return BadRequest("You cannot send messages to yourself");
+
+        var sender = await _userRepository.GetUserByUsernameAsync(username);
+        var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+        if (recipient == null) return NotFound();
+
+        //ef is not tracking this because we did not retrieve it
+        var message = new Message
+        {
+            //entity framework from take care of the ID fields for us based on the sender and recipient entity
+            Sender = sender,
+            Recipient = recipient,
+            //username will have to be set manually, EF doesn't take care of that
+            SenderUsername = sender.UserName,
+            RecipientUsername = recipient.UserName,
+            Content = createMessageDto.Content
+        };
+        
+        //use the context .add to track it
+        _messageRepository.AddMessage(message);
+
+        if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+
+        return BadRequest("Failed to send message");
+    }
+}
