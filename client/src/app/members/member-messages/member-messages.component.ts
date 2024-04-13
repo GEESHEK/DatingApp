34fa7cad9c23fe59ad89c5b772777ce81615978e
Gee Chong Shek <geeshek95@outlook.com>
2024-04-13 17:32:52 +0100

    Styling the message thread

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { TimeagoModule } from 'ngx-timeago';
 import { Message } from 'src/app/_models/message';
-import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
@@ -9,7 +9,7 @@ import { MessageService } from 'src/app/_services/message.service';
   standalone: true,
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, TimeagoModule]
 })
 export class MemberMessagesComponent implements OnInit {
   //child, get username from parent with Input
