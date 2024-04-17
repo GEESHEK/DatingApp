96cb719b2e7602ebe355ecfc172f2698737ad867
Gee Chong Shek <geeshek95@outlook.com>
2024-04-17 11:24:06 +0100

    Updating the seed method

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,15 +1,16 @@
 ﻿using System.Text.Json;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUser(DataContext context)
+    public static async Task SeedUser(UserManager<AppUser> userManager)
     {
         //check if our db has data in it, don't seed if we already do
-        if (await context.Users.AnyAsync()) return;
+        if (await userManager.Users.AnyAsync()) return;
         //read from the Json file
         var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
         //add this an lower case property names in the json will also work i.e. key 
@@ -20,10 +21,8 @@ public class Seed
         foreach (var user in users)
         {
             user.UserName = user.UserName.ToLower();
-
-            context.Users.Add(user);
+            //this will create and save in db
+            await userManager.CreateAsync(user, "Pa$$w0rd");
         }
-
-        await context.SaveChangesAsync();
     }
 }
